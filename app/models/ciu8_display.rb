0980da1df63d4e68b722afe72a17e3df1fa75e1d
Garry3Fingers <www.vlad78@yandex.ru>
2024-10-21 15:24:47 +0300

    Add validations to the Ciu8Display model

--- app/models/ciu8_display.rb
+++ app/models/ciu8_display.rb
@@ -1,2 +1,18 @@
 class Ciu8Display < ApplicationRecord
+  validates :transformer_substation, :adress_street, :meter_model,
+    :meter_serial_number, :display_serial_number, presence: {
+      message: "Пустое поле."
+    }
+
+  validates :meter_serial_number, :display_serial_number, numericality: {
+    only_integer: true, message: "Серийный номер должен состоять только из целых цифр 0-9."
+  }
+
+  validates :meter_serial_number, :display_serial_number, length: {
+    is: 8, message: "Длинна серийного номера должна состоять из %{count} цифр."
+  }
+
+  validates :meter_serial_number, :display_serial_number, uniqueness: {
+    message: "Серийный номер уже существует."
+  }
 end
