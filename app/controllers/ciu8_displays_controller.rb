9ba7bd4679159c2fb60ced8ca524ac959519aa1c
Garry3Fingers <www.vlad78@yandex.ru>
2024-10-23 14:48:05 +0300

    Add delete action to Ciu8DisplaysController

--- app/controllers/ciu8_displays_controller.rb
+++ app/controllers/ciu8_displays_controller.rb
@@ -11,7 +11,7 @@ class Ciu8DisplaysController < ApplicationController
     @ciu8_display = Ciu8Display.new(ciu8_display_params)
 
     if @ciu8_display.save
-      redirect_to "/"
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,10 +25,17 @@ class Ciu8DisplaysController < ApplicationController
     @ciu8_display = Ciu8Display.find(params[:id])
 
     if @ciu8_display.update(ciu8_display_params)
-      redirect_to "/"
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @ciu8_display = Ciu8Display.find(params[:id])
+    @ciu8_display.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
